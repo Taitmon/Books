8f969d3f2b967f70389414d0ac095000bbcaa125
taitmon <taitmonlynch@gmail.com>
2019-01-30 11:18:21 -0600

    check in

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,9 @@ public class Main
         Bookshelf bookshelf = new Bookshelf(125);
 
         System.out.println("The bookshelf contains " + bookshelf.bookCount + " books.");
+
+        Bookshelf bookshelf2 = new Bookshelf(50);
+
+        System.out.println("The bookshelf contains " + bookshelf2.bookCount + " books.");
     }
 }
