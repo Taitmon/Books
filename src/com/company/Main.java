c7fe893900e9e493b4cdbd7c995128cc1d6dfc41
taitmon <taitmonlynch@gmail.com>
2019-01-30 10:57:26 -0600

    check in

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Bookshelf bookshelf = new Bookshelf(125);
+
+        System.out.println("The bookshelf contains " + bookshelf.bookCount + " books.");
+    }
+}
